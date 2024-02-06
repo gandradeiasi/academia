958fec2353cb337ceadab8619cd9bbce507bbc61
Gabriel Iasi <31139106+gandradeiasi@users.noreply.github.com>
2024-02-06 15:47:59 -0300

    Add files via upload

---
+++ academia/script.js
@@ -0,0 +1,56 @@
+render();
+
+var botao_editar = document.querySelector("#editar");
+
+botao_editar.addEventListener(abrePopUpEditar);
+
+function render() {
+    var div_exercicios = document.querySelector('#div-exercicio');
+
+    var proximo_exercicio = obtemProximoExercicio();
+
+    if (!proximo_exercicio) {
+        div_exercicios.innerHTML = `<div class="mensagem-sem-exercicio">Sem exercícios</div>`;
+    }
+    else {
+        div_exercicios.innerHTML = `
+            <div class="nome-exercicio">
+                ${proximo_exercicio.nome}
+            </div>
+        `
+    }
+}
+
+function obtemProximoExercicio() {
+    var dados = ordenaExercicios();
+
+    var data_atual = new Date();
+
+    var exercícios_permitidos_hoje = dados.filter(function(exercicio) {
+        return exercicio.dia_permitido < data_atual;
+    });
+
+    return exercícios_permitidos_hoje[0];
+}
+
+function ordenaExercicios() {
+    var dados = getData();
+
+    var sorted_dados = dados.sort(function(a,b) {
+        return a.ultima_vez - b.ultima_vez
+    });
+
+    saveDados(sorted_dados);
+
+    return sorted_dados;
+}
+
+function saveDados(dados) {
+    if (!dados) return alert('Dados foram passados de forma incorreta na hora de salvar');
+    localStorage.dados = JSON.stringify(dados)
+}
+
+function getData() {
+    var dados = localStorage.dados ? JSON.parse(localStorage.dados) : [];
+    return dados;
+}
